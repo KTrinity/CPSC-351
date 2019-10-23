3d12669168c438ad4d58c726f4db5d174c44b607
KTrinity <35970207+KTrinity@users.noreply.github.com>
2019-10-23 12:39:32 -0700

    Add files via upload

--- cpsc351_assignment3_lewis_lam.cpp
+++ cpsc351_assignment3_lewis_lam.cpp
@@ -1,3 +1,6 @@
+//cpsc351_assignment3_lewis_lam.cpp : Synchronization
+//Copyright (c) Lewis Lam 10/24/2019
+
 #include <iostream>
 #include <fstream>
 #include <thread>
@@ -6,12 +9,12 @@
 
 using namespace std;
 
-void writeThread(string thread);
+void writeThread(string thread[]);
 mutex mtx;
 
 int main(int argc, const char* argv[]) {
-	string a = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26\n";
-	string b = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n";
+	string a[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26" };
+	string b[] = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" ,"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
 	thread threadA(writeThread, a);
 	thread threadB(writeThread, b);
@@ -22,12 +25,17 @@ int main(int argc, const char* argv[]) {
 	return 0;
 }
 
-void writeThread(string thread) {
+void writeThread(string thread[]) {
+	mtx.lock();
 	ofstream file;
 	file.open("synch.txt", fstream::app);
 	for (size_t i = 0; i < 20; i++) {
-		file << thread;
+		for (size_t j = 0; j < 26; j++) {
+			file << thread[j];
+			file << " ";
+		}
+		file << endl;
 	}
 	file.close();
-
+	mtx.unlock();
 }
